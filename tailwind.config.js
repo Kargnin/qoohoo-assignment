15a07b15551087e4fc1ff18ae31a544e8df4e6f3
Kargnin <bhushanmalani3@iitkgp.ac.in>
2022-10-25 19:19:07 +0530

    chore: :wrench: Added Oswald font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -8,6 +10,9 @@ module.exports = {
     extend: {
       colors: {
         'accent-1': '#333',
+      },
+      fontFamily: {
+        "Oswald": ["Oswald", ...defaultTheme.fontFamily.sans],
       },
     },
   },
